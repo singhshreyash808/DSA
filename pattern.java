6a090412fd33fc21691aff54e5e56f1df5f85391
singhshreyash808 <singhshreyash808@gmail.com>
2026-08-18 06:21:08 +0530

    Initial Commit-which have 3 Solid pattern question

---
+++ pattern.java
@@ -0,0 +1,52 @@
+//Solid Square Pattern 
+/*class Pattern{
+    public static void main(String args[]){
+        int n =4;
+        for( int row=1;row<=4;row++){
+            for(int col=1;col<=4;col++){
+                System.out.print("* ");
+            }
+       
+        System.out.println();
+         }
+    }
+}*/
+
+
+//Solid Rectangle pattern
+/*import java.util.Scanner;
+class Pattern{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of row:");
+        int n= sc.nextInt();
+
+        System.out.println("enter the value of coloumn:");
+        int  m= sc.nextInt();
+
+        for(int r=1;r<=n;r++){
+            for(int c =1;c<=m;c++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+
+    }
+}*/
+import java.util.Scanner;
+class Pattern{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of row:");
+        int n= sc.nextInt();
+
+        for(int r=1;r<=n;r++){
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
