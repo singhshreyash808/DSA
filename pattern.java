f85157f8b6cb61225874588232707ea85d3b1d87
singhshreyash808 <singhshreyash808@gmail.com>
2026-08-18 06:39:47 +0530

    Initial Commit with the Solid Rombus pattern

--- pattern.java
+++ pattern.java
@@ -52,3 +52,24 @@ class Pattern{
 
     }
 }*/
+
+import java.util.Scanner;
+class Pattern{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of row:");
+        int n= sc.nextInt();
+
+        for(int r=1;r<=n;r++)
+        {
+            for(int s=1;s<=n-r;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int c=1;c<=n;c++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
