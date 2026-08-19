0020433bbb6d5da0afb1ebf5f2f35e965cf36efc
singhshreyash808 <singhshreyash808@gmail.com>
2026-08-20 05:20:43 +0530

    Initial Commit with the hollow rectangle  pattern

--- pattern.java
+++ pattern.java
@@ -118,7 +118,7 @@ class Pattern{
    }*/
 
   //Inverted solid pyramid pattern
-  import java.util.Scanner;
+  /*import java.util.Scanner;
 
   class Pattern{
     public static void main(String args[]){
@@ -136,5 +136,45 @@ class Pattern{
             System.out.println();
         }
     }
-  }
+  }*/
+
+ //Hollow rectangle pattern
+            
+    
+/*import java.util.Scanner;
+
+class Pattern {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of row:");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the value of column:");
+        int m = sc.nextInt();
+
+        for (int r = 1; r <= n; r++) {
+
+            for (int c = 1; c <= m; c++) {
+
+                if (r == 1 || r == n || c == 1 || c == m) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            // Move to the next row
+            System.out.println();
+        }
+    }
+}*/
+            
+        
+       
+
+
+      
+
 
